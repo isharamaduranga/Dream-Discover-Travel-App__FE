801959e7baaf0882cda79d87680b47506d3d99c7
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2024-12-27 00:57:59 +0530

    implement new component apiConfig

---
+++ src/services/apiConfig.js
@@ -0,0 +1,10 @@
+/* http://13.201.57.37/api/v1/placesWithComments */
+const apiConfig = {
+    /*serverUrl: 'http://43.205.216.155',*/
+    serverUrl: 'http://127.0.0.1:8000',
+    basePath: 'api/v1',
+    port: 80
+}
+
+export default apiConfig
+
