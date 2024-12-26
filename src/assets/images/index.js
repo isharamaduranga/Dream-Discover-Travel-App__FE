d44f0055f4c68553761c42e32180f2d59e17d3ce
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2024-12-27 00:42:58 +0530

    create image accsess hub

---
+++ src/assets/images/index.js
@@ -0,0 +1,59 @@
+
+import banner from '../images/home/banner.png'
+import logo from '../images/logo/logo.ico'
+import facebook from './home/facebook-icon.png'
+import instagram from './home/instagram-icon.png'
+import twitter from './home/twitter-icon.png'
+import whatsapp from './home/whatsapp-icon.png'
+import linkedin from './home/linkedin-icon.png'
+import snapchat from './home/snapchat-icon.png'
+
+
+import adventure from './home/category/adventure.jpg'
+import wild_life from './home/category/wild_life.jpg'
+import water_sport from './home/category/water_sport.jpg'
+import nature from './home/category/nature.jpg'
+import camping from './home/category/camping.jpg'
+import ancient from './home/category/ancient.jpg'
+import festive from './home/category/festive.jpg'
+import travel_grid from './home/travel_grid.png'
+
+import adventure_cover from './category/adventure_cover.jpg'
+import wild_cover from './category/wild_cover.jpg'
+import water_sport_cover from './category/water_sport_cover.jpg'
+import nature_cover from './category/nature_cover.jpg'
+import camping_cover from './category/camping_cover.jpg'
+import ancient_cover from './category/ancient_cover.jpg'
+import festive_cover from './category/festive_cover.jpg'
+import emptyImg from './home/empty.jpg'
+
+export const Assets = {
+  logo:logo,
+  banner:banner,
+  facebook:facebook,
+  instagram:instagram,
+  twitter:twitter,
+  whatsapp:whatsapp,
+  linkedin:linkedin,
+  snapchat:snapchat,
+  travel_grid:travel_grid,
+
+
+  adventure:adventure,
+  wild_life:wild_life,
+  water_sport:water_sport,
+  nature:nature,
+  camping:camping,
+  ancient:ancient,
+  festive:festive,
+
+  adventure_cover:adventure_cover,
+  wild_cover:wild_cover,
+  water_sport_cover:water_sport_cover,
+  nature_cover:nature_cover,
+  camping_cover:camping_cover,
+  ancient_cover:ancient_cover,
+  festive_cover:festive_cover,
+  emptyImg:emptyImg
+
+}
