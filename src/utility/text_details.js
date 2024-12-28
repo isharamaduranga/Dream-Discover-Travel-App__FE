c69b3bf5d008b8e0c4f0ef062d790decf87fd6d2
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2024-12-28 13:28:20 +0530

    update text_details component and add  categoriesSingle & ratingRanges functions

--- src/utility/text_details.js
+++ src/utility/text_details.js
@@ -10,3 +10,22 @@ export  const badgeColorsArr = {
   Ancient: "light-warning",
   Festive: "light-secondary"
 }
+
+
+export const categoriesSingle = [
+  { value: 'Adventure', label: 'Adventure' },
+  { value: 'WildLife', label: 'WildLife' },
+  { value: 'WaterSport', label: 'WaterSport' },
+  { value: 'Nature', label: 'Nature' },
+  { value: 'Camping', label: 'Camping' },
+  { value: 'Ancient', label: 'Ancient' },
+  { value: 'Festive', label: 'Festive' }
+]
+
+export const ratingRanges = [
+  { value: { min:4.0, max:5.0 }, label: '5 Star' },
+  { value: { min:3.0, max:4.0 }, label: '4 Star' },
+  { value: { min:2.0, max:3.0 }, label: '3 Star' },
+  { value: { min:1.0, max:2.0 }, label: '2 Star' },
+  { value: { min:0.0, max:1.0 }, label: '1 Star' }
+]
