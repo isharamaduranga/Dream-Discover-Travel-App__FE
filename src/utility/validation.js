2ceed1e52ee08effe943a4d34a82c5fdba9ba9ea
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2024-12-28 13:03:22 +0530

    create new component validation.js and update few regex patterns

---
+++ src/utility/validation.js
@@ -0,0 +1,9 @@
+import toast from "react-hot-toast"
+
+export const DATE_REGEX = /^\d{4}-\d{2}-\d{2}$/
+export const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+export const PASSWORD_REGEX = /^.{5,}$/
+export const NAME_REGEX = /^[A-z ]+$/
+
+
+
