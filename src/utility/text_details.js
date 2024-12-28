ada16316cbbd838acfd4b82a503ba73215225a6d
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2024-12-28 13:26:52 +0530

    create text_details component and update badgeColorsArr

---
+++ src/utility/text_details.js
@@ -0,0 +1,12 @@
+import { Assets } from "@src/assets/images"
+
+
+export  const badgeColorsArr = {
+  Adventure: "light-info",
+  WildLife: "light-danger",
+  WaterSport: "light-primary",
+  Nature: "light-success",
+  Camping: "light-danger",
+  Ancient: "light-warning",
+  Festive: "light-secondary"
+}
