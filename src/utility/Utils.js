f8ff6eb403fe49057685bc231a1da2ef80f92900
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2024-12-28 13:14:38 +0530

    create new util.js for common methods for handle localstorage and user details

---
+++ src/utility/Utils.js
@@ -0,0 +1,36 @@
+import { DefaultRoute } from "../router/routes"
+
+// ** Checks if an object is empty (returns boolean)
+export const isObjEmpty = (obj) => Object.keys(obj).length === 0
+
+// ** Returns K format from a number
+export const kFormatter = (num) => (num > 999 ? `${(num / 1000).toFixed(1)}k` : num)
+
+export const getUserData = () => JSON.parse(localStorage.getItem("userData"))
+
+/**
+ ** This function is used for demo purpose route navigation
+ ** In real app you won't need this function because your app will navigate to same route for each users regardless of ability
+ ** Please note role field is just for showing purpose it's not used by anything in frontend
+ ** We are checking role just for ease
+ * ? NOTE: If you have different pages to navigate based on user ability then this function can be useful. However, you need to update it.
+ * @param {String} userRole Role of user
+ */
+export const getHomeRouteForLoggedInUser = (userRole) => {
+  if (userRole === "admin") return DefaultRoute
+  if (userRole === "client") return "/access-control"
+  return "/login"
+}
+
+// ** React Select Theme Colors
+export const selectThemeColors = (theme) => ({
+  ...theme,
+  colors: {
+    ...theme.colors,
+    primary25: "#7367f01a", // for option hover bg-color
+    primary: "#7367f0", // for selected option bg-color
+    neutral10: "#7367f0", // for tags bg-color
+    neutral20: "#ededed", // for input border-color
+    neutral30: "#ededed" // for input hover border-color
+  }
+})
